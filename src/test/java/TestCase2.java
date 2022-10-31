e02b1f90d0251aca70b2c5e8d3a242c5fb6d2734
kaherath <kaherath@wiley.com>
2022-10-31 23:30:07 +0530

    added project

---
+++ src/test/java/TestCase2.java
@@ -0,0 +1,40 @@
+import org.testng.annotations.Test;
+
+public class TestCase2 {
+
+    @Test
+    public void testA() {
+        System.out.println("This is test A");
+    }
+
+    @Test
+    public void testB() {
+        System.out.println("This is test B");
+    }
+
+    @Test
+    public void testC() {
+        System.out.println("This is test C");
+    }
+
+    @Test
+    public void testD() {
+        System.out.println("This is test D");
+    }
+
+    @Test
+    public void testE() {
+        System.out.println("This is test E");
+    }
+
+    @Test
+    public void testF() {
+        System.out.println("This is test F");
+    }
+
+    @Test
+    public void testG() {
+        System.out.println("This is test G");
+    }
+
+}
